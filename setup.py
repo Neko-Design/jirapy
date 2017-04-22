3ba52be01ba03fa2cdd8c1510cea9b8bff1f1143
Ewen McCahon <ewen.mccahon@gmail.com>
2017-04-22 17:40:28 +1000

    Getting ready for pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='jirapy',
+    packages=['jirapy'],
+    install_requires=[
+        'requests'
+    ],
+    version='0.1.dev0',
+    description='JIRA Data Wrapper for Easy WebHook Handling',
+    author='Ewen McCahon',
+    author_email='ewen.m.mccahon@student.uts.edu.au',
+    url='https://github.com/Neko-Design/jirapy',
+    keywords=['jira', 'api', 'wrapper', 'interface', 'webhooks', 'comments'],
+    classifiers=[],
+    license='APACHE2'
+)
